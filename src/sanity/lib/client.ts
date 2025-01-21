3d01d381f4d0053bc9acd1e757f2d948dcc8997f
ALI HAMZA NADEEM RANA <159229677+Alihamxa01@users.noreply.github.com>
2025-01-21 21:27:47 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -7,7 +7,7 @@ import { createClient } from 'next-sanity'
   dataset:"production",
   token :"skpuMMHlaESrkolGp636zL5N9pfBzoZVriORVuj0HQwr0UYeM9pg2U2qjDWKVnwlZRQWSQMQuD58MQYlEzN5dMXHgz31ZwyVKg1v1IrBapHwLvFbPVDtqyFzZ5YM9ZIyBzPV0oOw2QGGzTQg7dtltUSSwomqIQk71VvaotAKHm75LNJ2bzzW",
   apiVersion:"2025-01-15",
-  useCdn: false // Set to false if statically generating pages, using ISR or tag-based revalidation
+  useCdn: true // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 
 
